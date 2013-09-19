09400e76d313f37c862a58afd1adaa598851ceca
Rafael Rezende <rafaelrezend@gmail.com>
2013-09-19 04:11:51 +0200

    Changing build description

--- project/Build.scala
+++ project/Build.scala
@@ -8,10 +8,15 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
+    // Add your project dependencies here,
+    javaCore,
+    javaJdbc,
+    javaEbean,
     "postgresql" % "postgresql" % "8.4-702.jdbc4"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
   )
+
 }
